0a5a1ffe4011c8533363e7f61f27136a6399cf6e
tomek401273 <tomek371240@gmail.com>
2020-09-17 14:22:32 +0200

    init commit

---
+++ src/main/java/com/tgrajkowski/databinder/DataBinderApplication.java
@@ -0,0 +1,13 @@
+package com.tgrajkowski.databinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataBinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tgrajkowski/databinder/DataBinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgrajkowski.databinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
